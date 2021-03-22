fa2f38075c5148a99ae8464168f9366af7551fd8
alxdx <ustongs09@gmail.com>
2021-03-22 02:26:23 -0600

    agregada ruta /programa/<materia>

--- resources/programas.py
+++ resources/programas.py
@@ -1,31 +1,66 @@
 from flask import Response,request,make_response
 from flask_restful import Resource
 from database.models import Plan,Materia
+import re
+from bson import regex
 #clase que retorna todo el programa academico de una carrera
 #parametros que puede recibir son ITI || LCC || ICC
 class ProgramaCompleto(Resource):
     def get(self,carrera):
-        bas=Plan.objects.get(carrera=carrera+"-SEM-BASICO").to_json()
-        frm=Plan.objects.get(carrera=carrera+"-SEM-FORMATIVO").to_json()
-        opt=Plan.objects.get(carrera=carrera+"-SEM-OPTATIVAS").to_json()
-        return make_response(json.dumps(ans),200)
-        #return make_response(ans,200)
+        reg=regex.Regex.from_native(re.compile(carrera,re.IGNORECASE))
+        pipeline=[
+                {"$match":{"carrera":{"$regex":reg }}},
+                {"$sort":{"carrera":-1}},
+                {"$group":{
+                    "_id":0,
+                    "materias":{
+                        "$push":"$materias"
+                        }
+                    }
+                },
+                {"$project":{
+                    "_id":0,
+                    "carrera":carrera.upper(),
+                    "materias":{
+                        "$reduce":{
+                            "input":"$materias",
+                            "initialValue":[],
+                            "in":{"$concatArrays":["$$this","$$value"]}
+                            }
+                        }
+                    }
+                }]
+        ans=list(Plan.objects().aggregate(pipeline))
+        #return make_response(json.dumps(ans),200)
+        return make_response(ans[0],200)
 
 #clase que retorna todo el programa formativo de una carrera
 #clase que retorna todo el programa formativo de una carrera
 class ProgramaFormativo(Resource):
     def get(self,carrera):
-        ans=Plan.objects.get(carrera=carrera+"-SEM-FORMATIVO").to_json()
-        return make_response(ans,200)
+        pipeline=[
+            {"$match":{"carrera":carrera.upper()+"-SEM-FORMATIVO"}},
+            {"$project":{"_id":0,"carrera":1,"materias":1}}
+            ]
+        ans=list(Plan.objects.aggregate(pipeline))
+        return make_response(ans[0],200)
 #clase que retorna todo el programa basico de una carrera
 class ProgramaBasico(Resource):
     def get(self,carrera):
-        ans=Plan.objects.get(carrera=carrera+"-SEM-BASICO").to_json()
-        return make_response(ans,200)
+        pipeline=[
+            {"$match":{"carrera":carrera.upper()+"-SEM-BASICO"}},
+            {"$project":{"_id":0,"carrera":1,"materias":1}}
+            ]
+        ans=list(Plan.objects.aggregate(pipeline))
+        return make_response(ans[0],200)
 #
 #clase que retorna todo el programa de optativas de una carrera
 class ProgramaOptativas(Resource):
     def get(self,carrera):
-        ans=Plan.objects.get(carrera=carrera+"-SEM-OPTATIVAS").to_json()
-        return make_response(ans,200)
+        pipeline=[
+                {"$match":{"carrera":carrera.upper()+"-SEM-OPTATIVAS"}},
+            {"$project":{"_id":0,"carrera":1,"materias":1}}
+            ]
+        ans=list(Plan.objects.aggregate(pipeline))
+        return make_response(ans[0],200)
 
--- resources/routes.py
+++ resources/routes.py
@@ -5,7 +5,7 @@ def initialize_routes(api):
     api.add_resource(StatusTest,"/")
     api.add_resource(SignupApi,"/api/auth/signup")
     api.add_resource(LoginApi,"/api/auth/login")
-    #api.add_resource(ProgramaCompleto,"/api/programa/<carrera>" )
+    api.add_resource(ProgramaCompleto,"/api/programa/<carrera>" )
     api.add_resource(ProgramaBasico,"/api/prog-basico/<carrera>")
     api.add_resource(ProgramaFormativo,"/api/prog-formativo/<carrera>")
     api.add_resource(ProgramaOptativas,"/api/prog-optativas/<carrera>")
